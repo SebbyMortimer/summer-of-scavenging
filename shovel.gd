898c66a59b99901d4be76103d4dac3fe50bef687
Adam Steven <93397904+SebbyMortimer@users.noreply.github.com>
2025-07-14 21:09:12 +0100

    Init project to github

---
+++ shovel.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+const RAY_LENGTH = 1000
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+
+func _physics_process(delta):
+	var space_state = get_world_3d().direct_space_state
+	var cam = $"../Camera3D"
+	var mousepos = get_viewport().get_mouse_position()
+
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * RAY_LENGTH
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
+	
+	if result.has("position"):
+		position = result.position
+	else:
+		printerr("Mouse raycast didn't hit anything")
+		return
+	
+	if Input.is_action_just_pressed("mouse_click_left"):
+		var newHole = $"../Hole".duplicate()
+		newHole.position = Vector3(result.position.x, 0, result.position.z)
+		get_parent().add_child(newHole)
