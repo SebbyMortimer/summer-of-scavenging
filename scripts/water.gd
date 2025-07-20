a9b227c7f7cd28d3d139f8fed2949e2bb8d3e415
Adam Steven <93397904+SebbyMortimer@users.noreply.github.com>
2025-07-21 00:00:52 +0100

    Fix capitalisation - it annoyed me

--- scripts/water.gd
+++ scripts/water.gd
@@ -1,7 +1,7 @@
 extends StaticBody3D
 
-const speed = 0.5
+const SPEED = 0.5
 
 
 func _process(delta: float) -> void:
-	$MeshInstance3D.mesh.material.uv1_offset += Vector3(1, 1, 0) * speed * delta
+	$MeshInstance3D.mesh.material.uv1_offset += Vector3(1, 1, 0) * SPEED * delta
